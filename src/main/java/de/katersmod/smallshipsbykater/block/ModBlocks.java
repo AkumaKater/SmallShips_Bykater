07a301818cd96348363dd605453913e48767d7b6
Kater <jan.fortowski@gmail.com>
2022-05-19 22:12:50 +0200

    Mytril Block

--- src/main/java/de/katersmod/smallshipsbykater/block/ModBlocks.java
+++ src/main/java/de/katersmod/smallshipsbykater/block/ModBlocks.java
@@ -2,7 +2,9 @@ package de.katersmod.smallshipsbykater.block;
 
 import de.katersmod.smallshipsbykater.shipbyKaterMod;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
+import net.minecraft.block.Material;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
@@ -10,6 +12,9 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class ModBlocks {
+
+    public static final Block MYTHRIL_BLOCK = registerBlock("mythril_block",
+            new Block(FabricBlockSettings.of(Material.METAL).luminance(13).strength(6f).requiresTool()), ItemGroup.MISC);
     private static Block registerBlock(String name, Block block, ItemGroup group){
         registerBlockItem(name, block, group);
         return Registry.register(Registry.BLOCK, new Identifier(shipbyKaterMod.MOD_ID, name), block);
