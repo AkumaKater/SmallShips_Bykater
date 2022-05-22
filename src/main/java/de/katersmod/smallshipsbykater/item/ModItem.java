a54bb4de53c0f3f15912aba764e09da87cfec54b
Kater <jan.fortowski@gmail.com>
2022-05-22 12:34:11 +0200

    Lettuce, Tomato and a combination

--- src/main/java/de/katersmod/smallshipsbykater/item/ModItem.java
+++ src/main/java/de/katersmod/smallshipsbykater/item/ModItem.java
@@ -16,6 +16,15 @@ public class ModItem {
     public static final Item BURGER = registerItem("burger",
             new Item(new FabricItemSettings().group(ShipsItemGroup.Ships)));
 
+    public static final Item LETTUCE = registerItem("lettuce",
+            new Item(new FabricItemSettings().group(ShipsItemGroup.Ships)));
+
+    public static final Item TOMATO = registerItem("tomato",
+            new Item(new FabricItemSettings().group(ShipsItemGroup.Ships)));
+
+    public static final Item TOMATO_LETTUCE = registerItem("tomato_lettuce",
+            new Item(new FabricItemSettings().group(ShipsItemGroup.Ships)));
+
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(shipbyKaterMod.MOD_ID, name), item);
